d7ee933f795ad39376cb5bfb24998373d6da414d
Bob The Buidler <bobthebuidlerdefi@gmail.com>
2022-01-12 20:56:58 +0000

    chore: refactor yrs

--- yrs.py
+++ yrs.py
@@ -9,38 +9,8 @@ from lots import delete_active_lot, record_spent_lot, unspent_lots_for_export
 from transactions import transactions, tx_list_for_export, unique_tokens_sold
 
 
-def main():
-    def process_sale():
-      nonlocal unspent_lots
-      # cache these so we can manipulate them later
-      sold_amount = row.amount
-      sold_value_usd = row.value_usd
-      sold_gas_used = row.gas_used
-      
-      #start
-      while True:
-        active_lot = unspent_lots.iloc[0]
-        assert active_lot.timestamp <= row.timestamp
-        
-        if sold_amount > active_lot.amount:
-          taxable_events.append(process_portion_of_sale(sold_amount, row, active_lot))
-          
-          # this taxable event is not fully resolved but you used the whole active lot
-          unspent_lots = delete_active_lot(unspent_lots)
-          
-          # you still have some tokens left to calc cost basis for
-          sold_amount -= active_lot.amount
-          sold_value_usd -= row.price * active_lot.amount
-          sold_gas_used -= active_lot.gas_used
-
-          # run loop again
-          
-        else:
-          taxable_events.append(process_entire_sale(sold_amount, row, active_lot, sold_value_usd, sold_gas_used))
-          record_spent_lot(unspent_lots, active_lot, sold_amount, sold_gas_used)
-          # on to the next one
-          return
-    
+def main():    
+    METHOD = method_input()
     GOOD_ADDRESSES, BAD_ADDRESSES = address_inputs()
     _in, _out = transactions(GOOD_ADDRESSES)
     
@@ -52,32 +22,67 @@ def main():
       unspent_lots = _in[_in['vault'] == vault]
       
       # Filter out transfers to self, those are not taxable events
-      spent_lots = spent_lots[~spent_lots['to'].isin(GOOD_ADDRESSES)]
-      unspent_lots = unspent_lots[~unspent_lots['from'].isin(GOOD_ADDRESSES)]
+      spent_lots = spent_lots[~spent_lots['to_address'].isin(GOOD_ADDRESSES)]
+      unspent_lots = unspent_lots[~unspent_lots['from_address'].isin(GOOD_ADDRESSES)]
       
       # sort spent lots from least to most recent
-      spent_lots = spent_lots.sort_values(by='timestamp',ascending=True).reset_index(drop=True)
+      spent_lots = spent_lots.sort_values(by='block',ascending=True).reset_index(drop=True)
               
       # sort unspent lots according to selected method
-      unspent_lots = unspent_lots.sort_values(by='timestamp',ascending=True if method_input() == 'FIFO' else False).reset_index(drop=True)
+      unspent_lots = unspent_lots.sort_values(by='block',ascending=True if METHOD == 'FIFO' else False).reset_index(drop=True)
       
       # process
-      for row in spent_lots.itertuples(): process_sale()
+      for row in spent_lots.itertuples(): taxable_events, unspent_lots = process_sale(row, METHOD, taxable_events, unspent_lots)
       
       # record all lots still unsold    
       leftover_unspent_lots = pd.concat([leftover_unspent_lots, unspent_lots]).reset_index(drop=True)
         
-    # convert to pretty dict
-    result = pd.DataFrame(taxable_events).to_dict()
+    # prepare dict
+    result = {}
+    result['taxable events'] = pd.DataFrame(taxable_events).to_dict()
     
     # add some extra info
     result['failures'] = BAD_ADDRESSES
-    result['unspent tax lots'] = unspent_lots_for_export(leftover_unspent_lots)
-    try: result['tx list'] = tx_list_for_export(_in, _out)
-    except: pass
+    result['unspent lots'] = unspent_lots_for_export(leftover_unspent_lots)
+    result['transactions'] = tx_list_for_export(_in, _out)
     
     # voila
     return result
+
+def process_sale(row, method, unspent_lots, taxable_events):
+    # cache these so we can manipulate them later
+    sold_amount = row.amount
+    sold_value_usd = row.value_usd
+    sold_gas_used = row.gas_used
+    
+    #start
+    while True:
+      if method == 'LIFO':
+        unspent_lots = unspent_lots.sort_values(by='block',ascending=False)
+        temp = unspent_lots[unspent_lots['block'] >= row.block]          
+        unspent_lots = unspent_lots[unspent_lots['block'] < row.block]
+        unspent_lots = pd.concat([unspent_lots, temp])
+        
+      active_lot = unspent_lots.iloc[0]
+      assert active_lot.timestamp <= row.timestamp
+      if sold_amount > active_lot.amount:
+        taxable_events.append(process_portion_of_sale(sold_amount, row, active_lot))
+        
+        # this taxable event is not fully resolved but you used the whole active lot
+        unspent_lots = delete_active_lot(unspent_lots)
+        
+        # you still have some tokens left to calc cost basis for
+        sold_amount -= active_lot.amount
+        sold_value_usd -= row.price * active_lot.amount
+        sold_gas_used -= active_lot.gas_used
+
+        # run loop again
+        
+      else:
+        taxable_events.append(process_entire_sale(sold_amount, row, active_lot, sold_value_usd, sold_gas_used))
+        record_spent_lot(unspent_lots, active_lot, sold_amount, sold_gas_used)
+        # on to the next one
+        return taxable_events, unspent_lots
   
 def process_portion_of_sale(sold_amount, row, active_lot):
     duration, period = get_duration(row, active_lot)
